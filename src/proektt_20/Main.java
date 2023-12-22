fa9e2a627a2556cf4aa4acebe6c66525721cc75d
SerafimovaD <daci.serafimova@gmail.com>
2023-12-22 15:23:04 +0100

    Add first java program

---
+++ src/proektt_20/Main.java
@@ -0,0 +1,18 @@
+package proektt_20;
+
+public class Main {
+	
+	    public static void main(String[] args) {
+	        // Дефинирање на два броја
+	        int firstNumber = 5;
+	        int secondNumber = 10;
+
+	        // Креирање инстанца од класата Calculator
+	        Calculator calculator = new Calculator(firstNumber, secondNumber);
+
+	        // Печатење на резултатот од собирањето
+	        System.out.println("Сумата на броевите е: " + calculator.addNumbers());
+	    }
+	}
+
+
